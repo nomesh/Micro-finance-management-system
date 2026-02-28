b23fa2739c2a4729cea1f275441bce9b0f0b2ae1
nomesh.desilva <nomesh.desilva@amusedgroup.com>
2026-02-28 12:20:14 +0530

    Update database script

--- database/mydb.sql
+++ database/mydb.sql
@@ -1,32 +1,19 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 10, 2020 at 11:55 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.3.9
+-- Microfinance Management System - Sri Lanka Edition
+-- Database: `mydb`
+-- Currency: LKR (Sri Lankan Rupees)
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Database: `mydb`
---
-
 -- --------------------------------------------------------
-
---
--- Table structure for table `customer`
---
 
 CREATE TABLE `customer` (
   `cus_id` int(11) NOT NULL,
@@ -43,24 +30,19 @@ CREATE TABLE `customer` (
   `date` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `customer`
---
-
 INSERT INTO `customer` (`cus_id`, `scheme_id`, `scheme_name`, `scheme_amount`, `installment_amount`, `cus_name`, `cus_contact`, `cus_address`, `cus_asset`, `asset_price`, `img`, `date`) VALUES
-(1, 1, 'Home Loan', 300000, 25000, 'Nishad', '01723456765', 'Tangail,Baniara', 'Mobile', 10000, '[000274].jpg', '2020-04-09'),
-(2, 1, 'Home Loan', 300000, 25000, 'Habib', '01723456765', 'Khulna', 'Bat', 10000, '[000040].jpg', '2020-04-09'),
-(3, 1, 'Home Loan', 300000, 25000, 'TOma', '01723456765', 'Gazipur', 'Laptop', 50000, '[000658].jpg', '2020-04-09'),
-(4, 2, 'Phone Loan', 20000, 3333.33, 'Rajon', ' 0172345655454', 'Tangail', 'Lighter', 10000, '[000279].jpg', '2020-04-09'),
-(5, 1, 'Home Loan', 300000, 25000, 'test ', '01723456765', 'Tangail,Baniara', 'Mobile', 250000, '[000033].jpg', '2020-04-10'),
-(6, 1, 'Home Loan', 300000, 25000, 'test ', '01723456765', 'Tangail,Baniara', 'Mobile', 250000, '[000033].jpg', '2020-04-10'),
-(7, 2, 'Phone Loan', 20000, 3333.33, 'test 2', '01723456765', 'Tangail,Baniara', 'Mobile', 250000, '[000033].jpg', '2020-04-10');
+(1, 1, 'Housing Loan', 500000, 41666.67, 'Kumara Perera', '0771234567', 'No 45, Galle Road, Colombo 03', 'Gold Jewelry', 150000, '[000274].jpg', '2025-01-15'),
+(2, 1, 'Housing Loan', 500000, 41666.67, 'Nimal Fernando', '0772345678', 'Temple Road, Kandy', 'Motorcycle', 200000, '[000040].jpg', '2025-01-20'),
+(3, 2, 'Business Loan', 300000, 50000, 'Sanduni Silva', '0773456789', 'Main Street, Galle', 'Shop Equipment', 180000, '[000658].jpg', '2025-02-01'),
+(4, 3, 'Education Loan', 150000, 25000, 'Kasun Rajapaksa', '0774567890', 'Station Road, Negombo', 'Laptop & Books', 80000, '[000279].jpg', '2025-02-10'),
+(5, 2, 'Business Loan', 300000, 50000, 'Chamari Wickramasinghe', '0775678901', 'Market Street, Matara', 'Sewing Machines', 120000, '[000033].jpg', '2025-02-15'),
+(6, 1, 'Housing Loan', 500000, 41666.67, 'Pradeep Jayawardena', '0776789012', 'Lake Road, Kurunegala', 'Land Title', 800000, '[000033].jpg', '2025-02-20'),
+(7, 3, 'Education Loan', 150000, 25000, 'Dilini Amarasinghe', '0777890123', 'School Lane, Anuradhapura', 'Gold Chain', 90000, '[000033].jpg', '2025-02-25'),
+(8, 4, 'Vehicle Loan', 800000, 66666.67, 'Roshan De Silva', '0778901234', 'Beach Road, Hikkaduwa', 'Three-Wheeler', 400000, 'download.jpg', '2026-01-05'),
+(9, 2, 'Business Loan', 300000, 50000, 'Malini Gunasekara', '0779012345', 'Temple Street, Jaffna', 'Refrigerator', 150000, 'images.jpg', '2026-01-15'),
+(10, 4, 'Vehicle Loan', 800000, 66666.67, 'Anil Bandara', '0770123456', 'Hill Street, Nuwara Eliya', 'Van', 500000, 'download (1).jpg', '2026-02-01');
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `installment`
---
 
 CREATE TABLE `installment` (
   `install_id` int(11) NOT NULL,
@@ -72,39 +54,21 @@ CREATE TABLE `installment` (
   `ins_date` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `installment`
---
-
 INSERT INTO `installment` (`install_id`, `cus_id`, `amount`, `remaining`, `status`, `fine`, `ins_date`) VALUES
-(1, 1, 25000, 275000, 'Late', 50, '2020-05-12'),
-(2, 3, 25000, 275000, 'Late', 50, '2020-05-15'),
-(3, 2, 25000, 275000, 'On Time', 0, '2020-05-08'),
-(4, 2, 25000, 250000, 'Late', 50, '2020-06-13'),
-(5, 3, 25000, 250000, 'Late', 50, '2020-06-13'),
-(6, 4, 3333.33, 16666.7, 'Late', 50, '2020-04-24'),
-(7, 4, 3333.33, 13333.4, 'On Time', 0, '2020-04-11'),
-(8, 4, 6666.66, 6666.74, 'Delayed', 66.6666, '2020-05-15'),
-(9, 4, 6666.66, 6666.74, 'Delayed', 66.6666, '2020-05-08'),
-(10, 4, 3333.33, 10000.1, 'On Time', 0, '2020-04-18'),
-(11, 4, 3333.33, 6666.77, 'On Time', 0, '2020-04-25'),
-(12, 2, 25000, 225000, 'On Time', 0, '2020-06-18'),
-(13, 2, 50000, 175000, 'Delayed', 500, '2020-09-24'),
-(14, 2, 50000, 125000, 'Delayed', 500, '2020-12-11'),
-(15, 3, 50000, 200000, 'Delayed', 500, '2020-08-07'),
-(16, 3, 50000, 150000, 'Delayed', 500, '2020-10-15'),
-(17, 3, 25000, 125000, 'Late', 50, '2020-11-07'),
-(18, 3, 25000, 100000, 'On Time', 0, '2020-12-05'),
-(19, 6, 25000, 275000, 'Late', 50, '2020-04-11'),
-(20, 6, 50000, 225000, 'Delayed', 500, '2020-06-13'),
-(21, 7, 3333.33, 16666.7, 'Late', 50, '2020-04-11'),
-(22, 6, 25000, 200000, 'Late', 50, '2020-04-11');
+(1, 1, 41666.67, 458333.33, 'On Time', 0, '2025-02-15'),
+(2, 1, 41666.67, 416666.66, 'On Time', 0, '2025-03-15'),
+(3, 2, 41666.67, 458333.33, 'Late', 833.33, '2025-03-05'),
+(4, 3, 50000, 250000, 'On Time', 0, '2025-03-01'),
+(5, 3, 50000, 200000, 'On Time', 0, '2025-04-01'),
+(6, 4, 25000, 125000, 'On Time', 0, '2025-03-10'),
+(7, 5, 50000, 250000, 'Late', 1000, '2025-04-20'),
+(8, 6, 41666.67, 458333.33, 'On Time', 0, '2025-03-20'),
+(9, 7, 25000, 125000, 'On Time', 0, '2025-03-25'),
+(10, 8, 66666.67, 733333.33, 'On Time', 0, '2026-02-05'),
+(11, 9, 50000, 250000, 'Late', 1000, '2026-03-01'),
+(12, 10, 66666.67, 733333.33, 'On Time', 0, '2026-03-01');
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `loan_info`
---
 
 CREATE TABLE `loan_info` (
   `scheme_id` int(11) NOT NULL,
@@ -117,24 +81,19 @@ CREATE TABLE `loan_info` (
   `date` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `loan_info`
---
-
 INSERT INTO `loan_info` (`scheme_id`, `cus_id`, `scheme_amount`, `remaining_amount`, `installment_no`, `installment_remaining`, `installment_amount`, `date`) VALUES
-(1, 1, 300000, 275000, 12, 11, 25000, '2020-04-09'),
-(1, 2, 300000, 125000, 12, 5, 25000, '2020-04-09'),
-(1, 3, 300000, 100000, 12, 4, 25000, '2020-04-09'),
-(2, 4, 20000, 6666.77, 6, 2, 3333.33, '2020-04-09'),
-(1, 5, 300000, 300000, 12, 12, 25000, '2020-04-10'),
-(1, 6, 300000, 200000, 12, 8, 25000, '2020-04-10'),
-(2, 7, 20000, 16666.7, 6, 5, 3333.33, '2020-04-10');
+(1, 1, 500000, 416666.66, 12, 10, 41666.67, '2025-01-15'),
+(1, 2, 500000, 458333.33, 12, 11, 41666.67, '2025-01-20'),
+(2, 3, 300000, 200000, 6, 4, 50000, '2025-02-01'),
+(3, 4, 150000, 125000, 6, 5, 25000, '2025-02-10'),
+(2, 5, 300000, 250000, 6, 5, 50000, '2025-02-15'),
+(1, 6, 500000, 458333.33, 12, 11, 41666.67, '2025-02-20'),
+(3, 7, 150000, 125000, 6, 5, 25000, '2025-02-25'),
+(4, 8, 800000, 733333.33, 12, 11, 66666.67, '2026-01-05'),
+(2, 9, 300000, 250000, 6, 5, 50000, '2026-01-15'),
+(4, 10, 800000, 733333.33, 12, 11, 66666.67, '2026-02-01');
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `schedule`
---
 
 CREATE TABLE `schedule` (
   `install_no` int(11) NOT NULL,
@@ -144,89 +103,63 @@ CREATE TABLE `schedule` (
   `status` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `schedule`
---
-
 INSERT INTO `schedule` (`install_no`, `cus_id`, `cus_name`, `Time`, `status`) VALUES
-(1, 1, 'Nishad', '2020-05-09', 'paid'),
-(2, 1, 'Nishad', '2020-06-08', ''),
-(3, 1, 'Nishad', '2020-07-08', ''),
-(4, 1, 'Nishad', '2020-08-07', ''),
-(5, 1, 'Nishad', '2020-09-06', ''),
-(6, 1, 'Nishad', '2020-10-06', ''),
-(7, 1, 'Nishad', '2020-11-05', ''),
-(8, 1, 'Nishad', '2020-12-05', ''),
-(9, 1, 'Nishad', '2021-01-04', ''),
-(10, 1, 'Nishad', '2021-02-03', ''),
-(11, 1, 'Nishad', '2021-03-05', ''),
-(12, 1, 'Nishad', '2021-04-04', ''),
-(1, 2, 'Habib', '2020-05-09', 'paid'),
-(2, 2, 'Habib', '2020-06-08', 'paid'),
-(3, 2, 'Habib', '2020-07-08', 'paid'),
-(4, 2, 'Habib', '2020-08-07', 'paid'),
-(5, 2, 'Habib', '2020-09-06', 'unpaid'),
-(6, 2, 'Habib', '2020-10-06', 'paid'),
-(7, 2, 'Habib', '2020-11-05', 'paid'),
-(8, 2, 'Habib', '2020-12-05', 'unpaid'),
-(9, 2, 'Habib', '2021-01-04', 'unpaid'),
-(10, 2, 'Habib', '2021-02-03', 'unpaid'),
-(11, 2, 'Habib', '2021-03-05', 'unpaid'),
-(12, 2, 'Habib', '2021-04-04', 'unpaid'),
-(1, 3, 'TOma', '2020-05-09', 'paid'),
-(2, 3, 'TOma', '2020-06-08', 'unpaid'),
-(3, 3, 'TOma', '2020-07-08', 'paid'),
-(4, 3, 'TOma', '2020-08-07', 'paid'),
-(5, 3, 'TOma', '2020-09-06', 'paid'),
-(6, 3, 'TOma', '2020-10-06', 'paid'),
-(7, 3, 'TOma', '2020-11-05', 'paid'),
-(8, 3, 'TOma', '2020-12-05', 'paid'),
-(9, 3, 'TOma', '2021-01-04', 'unpaid'),
-(10, 3, 'TOma', '2021-02-03', 'unpaid'),
-(11, 3, 'TOma', '2021-03-05', 'unpaid'),
-(12, 3, 'TOma', '2021-04-04', 'unpaid'),
-(1, 4, 'Rajon', '2020-04-16', 'paid'),
-(2, 4, 'Rajon', '2020-04-23', 'unpaid'),
-(3, 4, 'Rajon', '2020-04-30', 'unpaid'),
-(4, 4, 'Rajon', '2020-05-07', 'unpaid'),
-(5, 4, 'Rajon', '2020-05-14', 'unpaid'),
-(6, 4, 'Rajon', '2020-05-21', 'unpaid'),
-(1, 5, 'test ', '2020-04-11', 'unpaid'),
-(2, 5, 'test ', '2020-04-12', 'unpaid'),
-(3, 5, 'test ', '2020-04-13', 'unpaid'),
-(4, 5, 'test ', '2020-04-14', 'unpaid'),
-(5, 5, 'test ', '2020-04-15', 'unpaid'),
-(6, 5, 'test ', '2020-04-16', 'unpaid'),
-(7, 5, 'test ', '2020-04-17', 'unpaid'),
-(8, 5, 'test ', '2020-04-18', 'unpaid'),
-(9, 5, 'test ', '2020-04-19', 'unpaid'),
-(10, 5, 'test ', '2020-04-20', 'unpaid'),
-(11, 5, 'test ', '2020-04-21', 'unpaid'),
-(12, 5, 'test ', '2020-04-22', 'unpaid'),
-(1, 6, 'test ', '2020-04-10', 'paid'),
-(2, 6, 'test ', '2020-04-10', 'paid'),
-(3, 6, 'test ', '2020-04-10', 'paid'),
-(4, 6, 'test ', '2020-04-10', 'paid'),
-(5, 6, 'test ', '2020-04-10', 'unpaid'),
-(6, 6, 'test ', '2020-04-10', 'unpaid'),
-(7, 6, 'test ', '2020-04-10', 'unpaid'),
-(8, 6, 'test ', '2020-04-10', 'unpaid'),
-(9, 6, 'test ', '2020-04-10', 'unpaid'),
-(10, 6, 'test ', '2020-04-10', 'unpaid'),
-(11, 6, 'test ', '2020-04-10', 'unpaid'),
-(12, 6, 'test ', '2020-04-10', 'unpaid'),
-(1, 7, 'test 2', '2020-04-10', 'paid'),
-(2, 7, 'test 2', '2020-04-10', 'unpaid'),
-(3, 7, 'test 2', '2020-04-10', 'unpaid'),
-(4, 7, 'test 2', '2020-04-10', 'unpaid'),
-(5, 7, 'test 2', '2020-04-10', 'unpaid'),
-(6, 7, 'test 2', '2020-04-10', 'unpaid');
+(1, 1, 'Kumara Perera', '2025-02-15', 'paid'),
+(2, 1, 'Kumara Perera', '2025-03-15', 'paid'),
+(3, 1, 'Kumara Perera', '2025-04-15', 'unpaid'),
+(4, 1, 'Kumara Perera', '2025-05-15', 'unpaid'),
+(5, 1, 'Kumara Perera', '2025-06-15', 'unpaid'),
+(6, 1, 'Kumara Perera', '2025-07-15', 'unpaid'),
+(7, 1, 'Kumara Perera', '2025-08-15', 'unpaid'),
+(8, 1, 'Kumara Perera', '2025-09-15', 'unpaid'),
+(9, 1, 'Kumara Perera', '2025-10-15', 'unpaid'),
+(10, 1, 'Kumara Perera', '2025-11-15', 'unpaid'),
+(11, 1, 'Kumara Perera', '2025-12-15', 'unpaid'),
+(12, 1, 'Kumara Perera', '2026-01-15', 'unpaid'),
+(1, 2, 'Nimal Fernando', '2025-02-20', 'unpaid'),
+(2, 2, 'Nimal Fernando', '2025-03-20', 'paid'),
+(3, 2, 'Nimal Fernando', '2025-04-20', 'unpaid'),
+(4, 2, 'Nimal Fernando', '2025-05-20', 'unpaid'),
+(5, 2, 'Nimal Fernando', '2025-06-20', 'unpaid'),
+(6, 2, 'Nimal Fernando', '2025-07-20', 'unpaid'),
+(7, 2, 'Nimal Fernando', '2025-08-20', 'unpaid'),
+(8, 2, 'Nimal Fernando', '2025-09-20', 'unpaid'),
+(9, 2, 'Nimal Fernando', '2025-10-20', 'unpaid'),
+(10, 2, 'Nimal Fernando', '2025-11-20', 'unpaid'),
+(11, 2, 'Nimal Fernando', '2025-12-20', 'unpaid'),
+(12, 2, 'Nimal Fernando', '2026-01-20', 'unpaid'),
+(1, 3, 'Sanduni Silva', '2025-03-01', 'paid'),
+(2, 3, 'Sanduni Silva', '2025-04-01', 'paid'),
+(3, 3, 'Sanduni Silva', '2025-05-01', 'unpaid'),
+(4, 3, 'Sanduni Silva', '2025-06-01', 'unpaid'),
+(5, 3, 'Sanduni Silva', '2025-07-01', 'unpaid'),
+(6, 3, 'Sanduni Silva', '2025-08-01', 'unpaid'),
+(1, 4, 'Kasun Rajapaksa', '2025-03-10', 'paid'),
+(2, 4, 'Kasun Rajapaksa', '2025-04-10', 'unpaid'),
+(3, 4, 'Kasun Rajapaksa', '2025-05-10', 'unpaid'),
+(4, 4, 'Kasun Rajapaksa', '2025-06-10', 'unpaid'),
+(5, 4, 'Kasun Rajapaksa', '2025-07-10', 'unpaid'),
+(6, 4, 'Kasun Rajapaksa', '2025-08-10', 'unpaid'),
+(1, 5, 'Chamari Wickramasinghe', '2025-03-15', 'unpaid'),
+(2, 5, 'Chamari Wickramasinghe', '2025-04-15', 'paid'),
+(3, 5, 'Chamari Wickramasinghe', '2025-05-15', 'unpaid'),
+(4, 5, 'Chamari Wickramasinghe', '2025-06-15', 'unpaid'),
+(5, 5, 'Chamari Wickramasinghe', '2025-07-15', 'unpaid'),
+(6, 5, 'Chamari Wickramasinghe', '2025-08-15', 'unpaid'),
+(1, 8, 'Roshan De Silva', '2026-02-05', 'paid'),
+(2, 8, 'Roshan De Silva', '2026-03-05', 'unpaid'),
+(3, 8, 'Roshan De Silva', '2026-04-05', 'unpaid'),
+(4, 8, 'Roshan De Silva', '2026-05-05', 'unpaid'),
+(5, 8, 'Roshan De Silva', '2026-06-05', 'unpaid'),
+(6, 8, 'Roshan De Silva', '2026-07-05', 'unpaid'),
+(7, 8, 'Roshan De Silva', '2026-08-05', 'unpaid'),
+(8, 8, 'Roshan De Silva', '2026-09-05', 'unpaid'),
+(9, 8, 'Roshan De Silva', '2026-10-05', 'unpaid'),
+(10, 8, 'Roshan De Silva', '2026-11-05', 'unpaid'),
+(11, 8, 'Roshan De Silva', '2026-12-05', 'unpaid'),
+(12, 8, 'Roshan De Silva', '2027-01-05', 'unpaid');
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `scheme`
---
 
 CREATE TABLE `scheme` (
   `scheme_id` int(11) NOT NULL,
@@ -239,19 +172,13 @@ CREATE TABLE `scheme` (
   `date` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `scheme`
---
-
 INSERT INTO `scheme` (`scheme_id`, `name`, `amount`, `r_asset`, `no_installment`, `Install_amount`, `duration`, `date`) VALUES
-(1, 'Home Loan', 300000, 10000, 12, 25000, 'month', '2020-04-09 '),
-(2, 'Phone Loan', 20000, 5000, 6, 3333.33, 'week', '2020-04-09 ');
+(1, 'Housing Loan', 500000, 100000, 12, 41666.67, 'month', '2025-01-01'),
+(2, 'Business Loan', 300000, 80000, 6, 50000, 'month', '2025-01-01'),
+(3, 'Education Loan', 150000, 50000, 6, 25000, 'month', '2025-01-01'),
+(4, 'Vehicle Loan', 800000, 200000, 12, 66666.67, 'month', '2026-01-01');
 
 -- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
@@ -263,102 +190,49 @@ CREATE TABLE `users` (
   `time` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `users`
---
-
 INSERT INTO `users` (`id`, `name`, `email`, `father_name`, `likee`, `pass`, `time`) VALUES
-(4, 'test1', 'test1@gmail.com', '', '', '12345', '2020-02-18 12:56:14'),
-(5, 'Nishad', 'k@gmail.com', '', '', '12345', '2020-02-18 1:07:20'),
-(6, 'Nishad', 'k@gmail.com', '', '', '12345', '2020-02-18 1:07:43'),
-(25, 'Nishad', 'k@gmail.com', '', '', 'df', '2020-02-18 1:23:09'),
-(28, 'TOma', 'k@gmail.com', '', '', 'jjj', '2020-02-18 1:25:10'),
-(34, 'Nishad3', 'kkhnishad@gmail.com', '', '', '123', '2020-02-20 9:51:15'),
-(35, 'inventory', 'kkhnishad@gmail.com', '', '', 'bb', '2020-02-20 9:54:29'),
-(36, 'inventory', 'k@gmail.com', '', '', 'bb', '2020-02-20 9:55:34'),
-(37, 'Suleiman', 'mangnificent@hmmm', '', '', '12345', '2020-04-02'),
-(38, 'Nishad', 'nis@gmail.com', 'wakil', 'cric', '12345', '2020-04-05'),
-(39, 'Alvi', 'alvi4200@gmail.com', 'faruki', 'bal', '12345', '2020-04-05'),
-(40, 'Viper', 'kkkkhnishad@gmail.com', 'Wakil', 'cric', '12345', '2020-04-10'),
-(41, 'Alvi', 'alvi4201@gmail.com', 'faruki', 'bal', '12345', '2020-04-10');
+(1, 'Admin User', 'test1@gmail.com', '', '', '12345', '2025-01-01'),
+(2, 'Saman Kumara', 'saman@microfinance.lk', 'W.A. Kumara', 'cricket', '12345', '2025-01-15'),
+(3, 'Nadeeka Silva', 'nadeeka@microfinance.lk', 'H.M. Silva', 'reading', '12345', '2025-02-01');
 
---
--- Indexes for dumped tables
---
+-- --------------------------------------------------------
 
---
--- Indexes for table `customer`
---
 ALTER TABLE `customer`
   ADD PRIMARY KEY (`cus_id`),
   ADD KEY `scheme` (`scheme_id`);
 
---
--- Indexes for table `installment`
---
 ALTER TABLE `installment`
   ADD PRIMARY KEY (`install_id`);
 
---
--- Indexes for table `loan_info`
---
 ALTER TABLE `loan_info`
   ADD KEY `scheme_id` (`scheme_id`),
-  ADD KEY `cus_id` (`cus_id`),
-  ADD KEY `cus_id_2` (`cus_id`);
+  ADD KEY `cus_id` (`cus_id`);
 
---
--- Indexes for table `schedule`
---
 ALTER TABLE `schedule`
   ADD KEY `cus_id` (`cus_id`);
 
---
--- Indexes for table `scheme`
---
 ALTER TABLE `scheme`
   ADD PRIMARY KEY (`scheme_id`),
   ADD UNIQUE KEY `name` (`name`);
 
---
--- Indexes for table `users`
---
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `customer`
---
 ALTER TABLE `customer`
-  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `installment`
---
 ALTER TABLE `installment`
-  MODIFY `install_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `install_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
---
--- AUTO_INCREMENT for table `loan_info`
---
 ALTER TABLE `loan_info`
-  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `cus_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
---
--- AUTO_INCREMENT for table `scheme`
---
 ALTER TABLE `scheme`
-  MODIFY `scheme_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `scheme_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
---
--- AUTO_INCREMENT for table `users`
---
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=42;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
